e7ffa31ee1afeecde80be6dbaef7329bf9bf1d06
ruru <ruru.dev07@gmail.com>
2024-04-17 11:52:27 +0530

    feat: create navbar (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import { ModeToggle } from "@/components/ui/ModeToggle";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        let&apos;s get started 
-      </h1>
-      <ModeToggle />
+    <main className="mx-10 lg:mx-56">
+      <Navbar />
+      <section className=" h-screen flex items-center justify-center">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          let&apos;s get started
+        </h1>
+      </section>
     </main>
   );
 }
